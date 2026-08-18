c9712cd3d7cca1d9883317d125d6a507878dcf4e
jamesjt <jtjamesni@gmail.com>
2026-08-18 00:58:26 -0700

    fix(golem): 'on the' to image (840, 193)

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -364,8 +364,8 @@ export default {
     ontheStyle() {
       const { x, y, s } = this.bgA;
       return {
-        left: x + 830 * s + "px",
-        top: y + 205 * s + "px",
+        left: x + 840 * s + "px",
+        top: y + 193 * s + "px",
         fontSize: 26.4 * s + "px"
       };
     },
